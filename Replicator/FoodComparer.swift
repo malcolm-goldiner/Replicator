6c50ae6eba6efae858d995bcdea24bc827eca7f6
Malcolm Goldiner <mgoldiner@yahoo.com>
2017-05-29 00:20:38 -0400

    Meal Plan UI
    - Adding meal map
    - updating mostly unused xib

--- Replicator/FoodComparer.swift
+++ Replicator/FoodComparer.swift
@@ -23,7 +23,23 @@ extension FSFood
             
           
             
-            let resultsArr =  [ isNoMoreThanTen(x: Double(servingA.caloriesValue()), y: Double(servingB.caloriesValue())), isNoMoreThanTen(x: Double(servingA.carbohydrateValue()), y: Double(servingB.carbohydrateValue())), isWithinTen(x: Double(servingA.proteinValue()), y: Double(servingB.proteinValue()), nutrient:"protein"), isNoMoreThanTen(x: Double(servingA.fatValue()), y: Double(servingB.fatValue())), isNoMoreThanTen(x: Double(servingA.saturatedFatValue()), y: Double(servingB.saturatedFatValue())), isWithinTen(x: Double(servingA.polyunsaturatedFatValue()), y: Double(servingB.polyunsaturatedFatValue()), nutrient:"polyunsatured fat"), isWithinTen(x: Double(servingA.monounsaturatedFatValue()), y: Double(servingB.monounsaturatedFatValue()), nutrient: "monounsatured fat"), isNoMoreThanTen(x: Double(servingA.transFatValue()), y: Double(servingB.transFatValue())), isNoMoreThanTen(x: Double(servingA.cholesterolValue()), y: Double(servingB.cholesterolValue())), isWithinTen(x: Double(servingA.sodiumValue()), y: Double(servingB.sodiumValue()), nutrient: "sodium"), isWithinTen(x: Double(servingA.potassiumValue()), y: Double(servingB.potassiumValue()), nutrient: "potassium"), isWithinTen(x: Double(servingA.fiberValue()), y: Double(servingB.fiberValue()), nutrient: "fiber"), isWithinTen(x: Double(servingA.sugarValue()), y: Double(servingB.sugarValue()), nutrient: "sugar"), isWithinTen(x: Double(servingA.vitaminAValue()), y: Double(servingB.vitaminAValue()), nutrient: "vitamin a"), isWithinTen(x: Double(servingA.vitaminCValue()), y: Double(servingB.vitaminCValue()), nutrient: "vitamin c"), isWithinTen(x: Double(servingA.calciumValue()), y: Double(servingB.calciumValue()), nutrient: "calcium"), isWithinTen(x: Double(servingA.ironValue()), y: Double(servingB.ironValue()), nutrient: "iron")]
+            let resultsArr =  [ isWithinTen(x: Double(servingA.caloriesValue()), y: Double(servingB.caloriesValue()), nutrient: "calories"),
+                                isWithinTen(x: Double(servingA.carbohydrateValue()), y: Double(servingB.carbohydrateValue()), nutrient: "carbohydrate"),
+                                isWithinTen(x: Double(servingA.proteinValue()), y: Double(servingB.proteinValue()), nutrient:"protein"),
+                                isWithinTen(x: Double(servingA.fatValue()), y: Double(servingB.fatValue()), nutrient: "fat"),
+                                isWithinTen(x: Double(servingA.saturatedFatValue()), y: Double(servingB.saturatedFatValue()), nutrient: "saturatedFat"),
+                                isWithinTen(x: Double(servingA.polyunsaturatedFatValue()), y: Double(servingB.polyunsaturatedFatValue()), nutrient:"polyunsatured fat"),
+                                isWithinTen(x: Double(servingA.monounsaturatedFatValue()), y: Double(servingB.monounsaturatedFatValue()), nutrient: "monounsatured fat"),
+                                isWithinTen(x: Double(servingA.transFatValue()), y: Double(servingB.transFatValue()), nutrient: "trans fat"),
+                                isWithinTen(x: Double(servingA.cholesterolValue()), y: Double(servingB.cholesterolValue()), nutrient: "cholestrol"),
+                                isWithinTen(x: Double(servingA.sodiumValue()), y: Double(servingB.sodiumValue()), nutrient: "sodium"),
+                                isWithinTen(x: Double(servingA.potassiumValue()), y: Double(servingB.potassiumValue()), nutrient: "potassium"),
+                                isWithinTen(x: Double(servingA.fiberValue()), y: Double(servingB.fiberValue()), nutrient: "fiber"),
+                                isWithinTen(x: Double(servingA.sugarValue()), y: Double(servingB.sugarValue()), nutrient: "sugar"),
+                                isWithinTen(x: Double(servingA.vitaminAValue()), y: Double(servingB.vitaminAValue()), nutrient: "vitamin a"),
+                                isWithinTen(x: Double(servingA.vitaminCValue()), y: Double(servingB.vitaminCValue()), nutrient: "vitamin c"),
+                                isWithinTen(x: Double(servingA.calciumValue()), y: Double(servingB.calciumValue()), nutrient: "calcium"),
+                                isWithinTen(x: Double(servingA.ironValue()), y: Double(servingB.ironValue()), nutrient: "iron")]
             
             if resultsArr.count > 0
             {
@@ -54,16 +70,16 @@ extension FSFood
         return abs(x - y) <= margin
     }
     
-    func isNoMoreThanTen(x: Double, y:Double) -> Bool
-    {
-        var margin = 0.0
-        
-        margin = recommendedDailyAllowanceForNutrient(nutrient: nutrient)
-        
-        print("\(x) vs \(y)\n")
-        
-        return x > y || (y - x) <= margin
-    }
+//    func IsWithinTen(x: Double, y:Double) -> Bool
+//    {
+//        var margin = 0.0
+//        
+//        margin = recommendedDailyAllowanceForNutrient(nutrient: nutrient)
+//        
+//        print("\(x) vs \(y)\n")
+//        
+//        return x > y || (y - x) <= margin
+//    }
     
     // Values for men 18 - 50 in 2000 calorie diet
     func recommendedDailyAllowanceForNutrient(nutrient: String) -> Double
@@ -71,39 +87,39 @@ extension FSFood
         switch(nutrient)
         {
             case "calories":
-                return 0.1 * 2000
+                return 0.5 * 2000
             case "carbohydrates":
-                return 0.1 * 130
+                return 0.5 * 130
             case "protein":
-                return 0.1 * 56
+                return 0.5 * 56
             case "fat":
-                return 0.1 * 2
+                return 0.5 * 2
             case "saturated fat":
-                return 0.1 * 24
+                return 0.5 * 24
             case "polyunsaturated fat":
-                return 0.1 * 700
+                return 0.5 * 700
             case "monounsaturated fat":
-                return 0.1 * 55
+                return 0.5 * 55
             case "trans fat":
-                return 0.1 * 2
+                return 0.5 * 2
             case "cholestrol":
-                return 0.1 * 0.3
+                return 0.5 * 0.3
             case "sodium":
-                return 0.1 * 2.3
+                return 0.5 * 2.3
             case "potassium":
-                return 0.1 * 4.7
+                return 0.5 * 4.7
             case "fiber":
-                return 0.1 * 38
+                return 0.5 * 38
             case "sugar":
-                return 0.1 * 37.5
+                return 0.5 * 37.5
             case "vitamin c":
-                return 0.1 * 0.06
+                return 0.5 * 0.06
             case "vitamin a":
-                return 0.1 * 0.009
+                return 0.5 * 0.009
             case "calcium":
-                return 0.1 * 1
+                return 0.5 * 1
             case "iron":
-                return 0.1 * 0.019
+                return 0.5 * 0.019
             default:
                 return 10
         }
--- Replicator/MealPlan.swift
+++ Replicator/MealPlan.swift
@@ -15,9 +15,95 @@ class Meal
     var mealThumbnailImage: UIImage?
 }
 
-class MealPlan
+class MealPlan: UIView
 {
+    static let mealCircleRadius: CGFloat = 5.0
     var creatorName: String?
     var creatorThumbnailImage: UIImage?
     var meals: [Meal]?
+    let mapStartingPoint = CGPoint(x: 15, y: 50)
+    let mealCircleStartingAngle = 0
+    let mealCircleColor = UIColor.green
+    let lineColor = UIColor.black
+    let mealCircleToMealLabelHorizontalSpacing: CGFloat = 10
+    let mealCircleLineWidth: CGFloat = 3.0
+    let mealCircleEndingAngle = Double.pi * 2
+    let lineLength: CGFloat = 200
+    var currentCircleEndingPosition: CGPoint?
+    let mealLabelFontSize: CGFloat = 9.0
+    var mealCircleCircumference: CGFloat  {
+        get {
+            return MealPlan.mealCircleRadius * 2
+        }
+    }
+    
+    func addTestMeals() {
+        let testBreakfast = Meal()
+        testBreakfast.mealName = "Bacon, Egg, And Cheese Sandiwch"
+        
+        let testLunch = Meal()
+        testLunch.mealName = "Turkey, Provolone, Tomoato, and Avocado Sandwich"
+        
+        let testDinner = Meal()
+        testDinner.mealName = "Pork Chops, Mashed Potatoes, and Green Beans"
+        
+        meals = [testBreakfast, testLunch, testDinner]
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        addTestMeals()
+       
+        for meal in meals! {
+            if (currentCircleEndingPosition != nil) {
+                drawLineToNextMeal()
+            }
+
+            if (meal.mealName != nil) {
+                drawMealCircleForMeal(meal.mealName!)
+            }
+        }
+    }
+    
+    func drawLineToNextMeal() {
+        let linePath = UIBezierPath()
+        linePath.move(to: CGPoint(x:self.currentCircleEndingPosition!.x, y: currentCircleEndingPosition!.y))
+        linePath.addLine(to: CGPoint(x: self.currentCircleEndingPosition!.x, y: self.currentCircleEndingPosition!.y + self.lineLength))
+        linePath.close()
+        lineColor.set()
+        linePath.stroke()
+        linePath.fill()
+    }
+    
+    func drawMealCircleForMeal(_ foodString: String) {
+        
+        var circlePath: UIBezierPath
+        if (currentCircleEndingPosition == nil)
+        {
+            circlePath = UIBezierPath(arcCenter: CGPoint(x: mapStartingPoint.x,y: mapStartingPoint.y), radius: MealPlan.mealCircleRadius, startAngle: CGFloat(mealCircleStartingAngle), endAngle:CGFloat(mealCircleEndingAngle), clockwise: true)
+            
+            currentCircleEndingPosition = CGPoint(x: mapStartingPoint.x, y: mapStartingPoint.y + MealPlan.mealCircleRadius)
+        } else {
+             circlePath = UIBezierPath(arcCenter: CGPoint(x: currentCircleEndingPosition!.x, y: currentCircleEndingPosition!.y + lineLength), radius: CGFloat(MealPlan.mealCircleRadius), startAngle: CGFloat(mealCircleStartingAngle), endAngle:CGFloat(mealCircleEndingAngle), clockwise: true)
+            
+                currentCircleEndingPosition = CGPoint(x: mapStartingPoint.x, y: currentCircleEndingPosition!.y + lineLength + MealPlan.mealCircleRadius)
+        }
+       
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        
+        shapeLayer.fillColor = mealCircleColor.cgColor
+        shapeLayer.strokeColor = mealCircleColor.cgColor
+        shapeLayer.lineWidth = mealCircleLineWidth
+        
+        self.layer.addSublayer(shapeLayer)
+        
+        let mealLabel = UILabel()
+        mealLabel.text = foodString
+        mealLabel.font = UIFont.systemFont(ofSize: mealLabelFontSize)
+        mealLabel.sizeToFit()
+        mealLabel.frame = CGRect(x: mapStartingPoint.x + mealCircleCircumference + mealCircleToMealLabelHorizontalSpacing, y: currentCircleEndingPosition!.y  - MealPlan.mealCircleRadius - (mealLabel.frame.size.height / 2), width: mealLabel.frame.size.width, height: mealLabel.frame.size.height)
+        addSubview(mealLabel)
+    }
+    
 }
